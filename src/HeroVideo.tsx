361fa714cb1a76e92caa4e38f493868dc301e9b0
Martin Blasko <mblasko42@gmail.com>
2023-11-08 01:58:00 +0100

    tweaks

--- src/HeroVideo.tsx
+++ src/HeroVideo.tsx
@@ -1,37 +1,35 @@
-import  { useState, useRef } from "react";
+import { useState, useRef, CSSProperties } from "react";
 import "./style.css";
 
 const videos: string[] = [
   "https://download.samplelib.com/mp4/sample-5s.mp4",
+  // "https://download.samplelib.com/mp4/sample-5s.mp4",
   "https://filesamples.com/samples/video/mp4/sample_960x540.mp4"
 ];
 
 export const HeroVideo = () => {
   const [currentVideo, setCurrentVideo] = useState(0);
   const videoRef = useRef(null);
+  const [motion, setMotion] = useState<"slidein" | "slideout" | "playing">("slidein");
+
 
   const handleCanPlay = () => {
-    videoRef.current.style.animationName = "slidein";
-    videoRef.current.style.animationDuration = "2s";
-    videoRef.current.style.animationFillMode = "forward";
+    setMotion("slidein");
   };
 
   const handleEnded = () => {
-
-    videoRef.current.style.animationName = "slideout";
-    videoRef.current.style.animationDuration = "2s";
-    videoRef.current.style.animationFillMode = "forward"
+    setMotion("slideout");
     setTimeout(() => {
       setCurrentVideo((currentVideo + 1) % videos.length);
-      videoRef.current.style.animationName = "none";
-      videoRef.current.style.animationDuration = "0s";
-      videoRef.current.style.animationFillMode = "none";
-    }, 2000); 
-  };
+      videoRef.current.style.opacity = "0";
+    }, 900);
 
+  };
   return (
-    <div className="video-container">
+    <div
+      className="video-container">
       <video
+        className={motion}
         ref={videoRef}
         onCanPlay={handleCanPlay}
         onEnded={handleEnded}
@@ -42,6 +40,7 @@ export const HeroVideo = () => {
       >
         
       </video>
+      <div className={"fakeshadow " + motion} />
     </div>
   );
 };
